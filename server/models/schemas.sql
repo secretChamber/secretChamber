aa345cad1d663e416a53682330c1e4bf4b127980
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-03-14 12:28:19 -0700

    corrected mistake in schema syntax

--- server/models/schemas.sql
+++ server/models/schemas.sql
@@ -1,19 +1,16 @@
+DROP DATABASE naberle;
+
 CREATE DATABASE naberle;
+
 USE naberle;
 
 
--- NOTE: setting column values as NOT NULL allows the tables 
--- to take care of forcing the user to fill in all fields corretly
--- where we can return an 'error' if it has no value during the post request;
-
--- NOTE: use "NOW()" to insert a value in a timestamp column if we want to have timestamps;
 CREATE TABLE reported_issues (
   user_id INT UNSIGNED NOT NULL,
   location VARCHAR(100) NOT NULL,
   type ENUM ('type_1', 'type_2', 'type_3', 'type_4', 'type_5'),
   status ENUM ('stat_1', 'stat_2', 'stat_3', 'stat_4'),
-  -- time_reported TIMESTAMP,
-	rep_issue_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY;
+	rep_issue_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
@@ -21,19 +18,14 @@ CREATE TABLE reported_issues (
 CREATE TABLE users (
   username VARCHAR (20),
   password VARCHAR (20),
-  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY;
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
-
--- NOTE: "PRIMARY KEY (rep_issue_id, time_voted)" in the votes TABLE 
--- takes care of creating a key that is unique to each voting instance, 
--- and simplifies the search for whether a user already voted for an issue. 
 CREATE TABLE votes (
   rep_issue_id INT UNSIGNED NOT NULL,
   user_id INT UNSIGNED NOT NULL,
-  -- time_voted TIMESTAMP,
-  PRIMARY KEY (rep_issue_id, time_voted) 
+  vote_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
