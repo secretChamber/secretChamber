de0a1f933d2b039d91692232764e8cab57306b54
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-03-14 15:28:31 -0700

    made the username UNIQUE, insert and extract functionality progressing

--- server/schemas.sql
+++ server/schemas.sql
@@ -4,24 +4,20 @@ CREATE DATABASE naberle;
 
 USE naberle;
 
-
 CREATE TABLE reported_issues (
   user_id INT UNSIGNED NOT NULL,
   lat VARCHAR(100) NOT NULL,
   lng VARCHAR(100) NOT NULL,
-  type ENUM ('trash_pickup', 'road_work', 'traffic_sign'),
-  status ENUM ('reported', 'resolved'),
+  type ENUM ('Trash', 'Roadwork', 'Traffic Sign'),
+  status ENUM ('Reported', 'Resolved'),
 	rep_issue_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
-
-
 CREATE TABLE users (
-  username VARCHAR (20),
-  password VARCHAR (20),
+  username VARCHAR (20) NOT NULL UNIQUE,
+  password VARCHAR (20) NOT NULL,
   user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
-
 
 CREATE TABLE votes (
   rep_issue_id INT UNSIGNED NOT NULL,
