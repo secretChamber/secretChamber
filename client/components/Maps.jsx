c95a73cf701d7b2f98c4d1e98b80eb286d2613c1
anirvan90 <anirvan.awatramani@gmail.com>
2017-03-15 12:09:35 -0700

    info boxes show up

--- client/components/Maps.jsx
+++ client/components/Maps.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap, Marker} from 'react-google-maps';
+import {GoogleMapLoader, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 
 class Map extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      infoWindowShow: false
+    }
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.closeInfo = this.closeInfo.bind(this);
+  }
+  onMarkerClick (e) {
+    console.log(this.state.infoWindowShow);
+    this.setState({
+      infoWindowShow: true
+    });
+    console.log(this.state.infoWindowShow);
+  }
+  closeInfo (e) {
+    console.log('Hello');
+  }
+
   render () {
     const mapContainer = <div style={{height:'100%', width:'100%'}}></div>
 
@@ -12,7 +31,9 @@ class Map extends React.Component {
           lng: pin.location.lng
         }
       }
-      return <Marker key={i} {...marker} />
+      return <Marker key={i} {...marker} onClick={this.onMarkerClick}>
+        <InfoWindow onCloseClick={this.closeInfo} content={pin.reporter}/>
+      </Marker>
     })
     return (
       <GoogleMapLoader 
--- client/components/Places.jsx
+++ client/components/Places.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 class Places extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const issueList = this.props.markers.map((pin, i) => {
+      return(
+        <li key={i}>
+          <div>Issue - {pin.issue}</div>
+          <div>Reporter - {pin.reporter}</div>
+        </li>
+      )
+    })
     return (
-      <div>This Is a Places Component</div>
+      <div>
+        Issues
+        <ol>
+          {issueList}
+        </ol>
+      </div>
     )
   }
 }
